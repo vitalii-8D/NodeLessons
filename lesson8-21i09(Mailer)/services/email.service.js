256b9f07adc5e444ff8c80c78c10c5cecabee294
Vitalii <gotbubu0@gmail.com>
2020-09-27 20:01:17 +0300

    -- lesson 8: Ending

--- lesson8-21i09(Mailer)/services/email.service.js
+++ lesson8-21i09(Mailer)/services/email.service.js
@@ -26,7 +26,7 @@ class EmailService {
             const templateInfo = htmlTemplates[action];
 
             const html = await emailTemplates.render(templateInfo.templateFilename, {...context, frontendUrl: FRONTEND_URL});
-
+ 
             const mailOptions = {
                 from: 'NO REPLY CAR SHOP',
                 to: userMail,
